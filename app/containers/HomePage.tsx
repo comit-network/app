ee50c9a2093ac450a99cb6843cd2b3de711aac71
Yos Riady <yos.riady@tenx.tech>
2020-02-28 10:59:02 +0800

    Refactor Home container

--- app/containers/HomePage.tsx
+++ app/containers/HomePage.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
-import Home from '../components/Home';
+import React, { useEffect, useState } from 'react';
 
 export default function HomePage() {
-  return <Home />;
+  const [rate, setRate] = useState('Loading...');
+
+  useEffect(() => {
+    async function fetchRate() {
+      const res = await fetch('http://localhost:3000/rates');
+      const { rates } = await res.json();
+      setRate(rates.dai.btc);
+    }
+    fetchRate();
+  }, []);
+
+  return (
+    <div data-tid="container">
+      <h2>Home</h2>
+      <p>Rate: 1 BTC = {(1 / rate).toFixed(2)} DAI</p>
+
+      {/* <Link to="/counter">to Counter</Link> */}
+    </div>
+  );
 }
