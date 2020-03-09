6a01496fb2379dc70dbc2aae167744698813528b
Yos Riady <yos.riady@tenx.tech>
2020-03-09 16:53:29 +0800

    Implement GET / and GET /swaps, minor refactors

--- maker/services/root.js
+++ maker/services/root.js
@@ -1,14 +1,22 @@
 'use strict'
 
 const { name, version } = require("../package.json");
+const {
+  loadEnvironment,
+  getNode
+} = require('../comit');
 
 module.exports = function (fastify, opts, next) {
+  loadEnvironment(); // COMIT
+
   fastify.register(require('fastify-cors'), {
     // put your options here
   })
 
   fastify.get('/', async function (request, reply) {
-    return { name, version };
+    const { peerId, addressHint } = await getNode(0, 'Maker');
+
+    return { name, version, peerId, addressHint };
   })
 
   next()
