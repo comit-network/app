e7f38370e6b312a0fdccbdb2134e14c326b1825f
Yos Riady <yos.riady@tenx.tech>
2020-03-11 14:28:05 +0800

    Implement GET /swaps

--- maker/services/swaps/index.js
+++ maker/services/swaps/index.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const {
   getNode
 } = require('../../comit');
@@ -5,8 +6,19 @@ const {
 module.exports = function (fastify, opts, next) {
   fastify.get('/swaps', async function (request) {
     const maker = await getNode(0, 'Maker');
-    const swaps = await maker.comitClient.getOngoingSwaps();
-    return { swaps };
+    const newSwaps = await maker.comitClient.getNewSwaps();
+    const newSwapsDetails = await Promise.all(_.map(newSwaps, s => s.fetchDetails()));
+    const newSwapsProperties = _.map(newSwapsDetails, s => s['properties']);
+
+    const ongoingSwaps = await maker.comitClient.getOngoingSwaps();
+    const ongoingSwapsDetails = await Promise.all(_.map(ongoingSwaps, s => s.fetchDetails()));
+    const ongoingSwapsProperties = _.map(ongoingSwapsDetails, s => s['properties']);
+
+    const doneSwaps = await maker.comitClient.getDoneSwaps();
+    const doneSwapsDetails = await Promise.all(_.map(doneSwaps, s => s.fetchDetails()));
+    const doneSwapsProperties = _.map(doneSwapsDetails, s => s['properties']);
+
+    return { new: newSwapsProperties, ongoing: ongoingSwapsProperties, done: doneSwapsProperties };
   })
 
   next()
