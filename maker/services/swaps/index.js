be51966ec7b963570f1edccb05bc1ac26c003504
Yos Riady <yos.riady@tenx.tech>
2020-03-18 11:50:56 +0800

    Implement maker & taker state machine

--- maker/services/swaps/index.js
+++ maker/services/swaps/index.js
@@ -1,34 +1,9 @@
-const _ = require('lodash');
 const { NotFound } = require('http-errors')
 const {
-  getNode
+  getNode,
+  getSwaps,
+  findSwapById
 } = require('../../comit');
-
-async function parseProperties(swaps) {
-  const details = await Promise.all(_.map(swaps, s => s.fetchDetails()));
-  const properties = _.map(details, s => s['properties']);
-  return properties
-}
-
-async function getSwaps() {
-  const maker = await getNode(0, 'Maker');
-  const newSwaps = await maker.comitClient.getNewSwaps();
-  const newSwapsProperties = await parseProperties(newSwaps);
-
-  const ongoingSwaps = await maker.comitClient.getOngoingSwaps();
-  const ongoingSwapsProperties = await parseProperties(ongoingSwaps);
-
-  const doneSwaps = await maker.comitClient.getDoneSwaps();
-  const doneSwapsProperties = await await parseProperties(doneSwaps);
-
-  return [...newSwapsProperties, ...ongoingSwapsProperties, ...doneSwapsProperties];
-}
-
-async function findSwapById(id) {
-  const swaps = await getSwaps();
-  const withId = _.find(swaps, { id });
-  return withId;
-}
 
 module.exports = function (fastify, opts, next) {
   fastify.get('/swaps', async function (request) {
