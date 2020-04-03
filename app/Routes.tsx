2540de8cd3ed64e8f1e7039fc084427e06c0b77e
Yos Riady <yos.riady@tenx.tech>
2020-04-03 10:30:27 +0800

    Implement Wallet Store

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
 import SwapDetailsPage from './containers/SwapDetailsPage';
 
 export default function Routes() {
   return (
     <App>
       <Switch>
-        <Route path="/counter" component={CounterPage} />
         <Route path="/swaps/:id" component={SwapDetailsPage} />
         <Route path="/" component={HomePage} />
       </Switch>
