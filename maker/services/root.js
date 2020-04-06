635ed82e010f77975d1536c7c27485fb7dc0e2f3
Yos Riady <yos.riady@tenx.tech>
2020-04-06 12:44:53 +0800

    Refactor maker environment variables

--- maker/services/root.js
+++ maker/services/root.js
@@ -1,14 +1,12 @@
 'use strict'
 
+require('dotenv').config();
 const { name, version } = require("../package.json");
 const {
-  loadEnvironment,
   getNode
 } = require('../comit');
 
 module.exports = function (fastify, opts, next) {
-  loadEnvironment(); // COMIT
-
   fastify.register(require('fastify-cors'), {
     // put your options here
   })
