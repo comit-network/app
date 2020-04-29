fa504c4bb5578cdc6e995d18e9632336486116ed
Yos Riady <yos.riady@tenx.tech>
2020-04-29 16:52:50 +0800

    Implement useFetch hook with built-in error handling

--- app/hooks/useFetch.tsx
+++ app/hooks/useFetch.tsx
@@ -1,11 +1,38 @@
 import { useEffect, useReducer } from 'react';
 
-const useFetch = (url, initialData) => {
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.data
+      };
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const useFetch = (func, initialData) => {
   const [state, dispatch] = useReducer(dataFetchReducer, {
     isLoading: false,
     isError: false,
     data: initialData
   });
+  const [refreshFlag, refresh] = useReducer(r => r + 1, 0);
 
   useEffect(() => {
     let didCancel = false;
@@ -14,10 +41,10 @@ const useFetch = (url, initialData) => {
       dispatch({ type: 'FETCH_INIT' });
 
       try {
-        const result = await axios(url);
+        const result = await func();
 
         if (!didCancel) {
-          dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+          dispatch({ type: 'FETCH_SUCCESS', data: result });
         }
       } catch (error) {
         if (!didCancel) {
@@ -31,9 +58,9 @@ const useFetch = (url, initialData) => {
     return () => {
       didCancel = true;
     };
-  }, [url]);
+  }, [refreshFlag]); // TODO: enable refresh
 
-  return [state];
+  return { ...state, refresh };
 };
 
 export default useFetch;
