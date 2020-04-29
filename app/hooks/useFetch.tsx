0e9f409acc2570d1d4ab5b5e2e29da2e41a2ada7
Yos Riady <yos.riady@tenx.tech>
2020-04-29 16:16:46 +0800

    Implement rate refresh

---
+++ app/hooks/useFetch.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useReducer } from 'react';
+
+const useFetch = (url, initialData) => {
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData
+  });
+
+  useEffect(() => {
+    let didCancel = false;
+
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_INIT' });
+
+      try {
+        const result = await axios(url);
+
+        if (!didCancel) {
+          dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+        }
+      } catch (error) {
+        if (!didCancel) {
+          dispatch({ type: 'FETCH_FAILURE' });
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      didCancel = true;
+    };
+  }, [url]);
+
+  return [state];
+};
+
+export default useFetch;
